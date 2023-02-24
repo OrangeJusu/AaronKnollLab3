06262ba8aa66d7631eda6d0a8bd953c32ceb24e1
Aaron Knoll <91743185+OrangeJusu@users.noreply.github.com>
2023-02-24 11:05:15 -0600

    Update Stack.h

--- Stack.h
+++ Stack.h
@@ -11,61 +11,11 @@ private:
 	int maxItems;
 	int currentItems;
 public:
-	Stack(int maxI) {
-		maxItems = maxI;
-		currentItems = 0;
-		top = nullptr;
-		bot = nullptr;
-	}
-
-	bool isEmpty() {
-		if (currentItems == 0) {
-			return true;
-		}
-		return false;
-	}
-	bool isFull() {
-		if (currentItems == maxItems) {
-			return true;
-		}
-		return false;
-	}
-	void Push(int val) {
-		Item* item = new Item(val);
-		if (!isFull()) {
-			if (top == nullptr) {
-				top = item;
-				bot = top;
-			}
-			else {
-				bot->next = item;
-				bot = bot->next;
-			}
-			currentItems++;
-		}
-		else {
-			cout << "The List is Full!" << endl;
-		}
-	}
-	void Pop() {
-		if (!isEmpty()) {
-			top = top->next;
-			currentItems--;
-		}
-		else {
-			cout << "The List is Empty!" << endl;
-		}
-	}
-	Item* getTop() {
-		Item* copyTop = top;
-		return copyTop;
-	}
-	void display() {
-		Item* temp = top;
-		for (int i = 0; i < currentItems; i++) {
-			cout << temp->data << endl;
-			temp = temp->next;
-		}
-		cout << endl;
-	}
+	Stack(int maxI);
+	bool isEmpty();
+	bool isFull();
+	void Push(int val);
+	void Pop();
+	Item* getTop();
+	void display();
 };
